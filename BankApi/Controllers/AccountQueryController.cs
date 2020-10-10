efea37143ba55c40e99324215e91dcbef18af708
Gabriel Augusto <augusto_costa96@hotmail.com>
2020-10-10 13:52:25 -0300

    Adding unit testing infrastructure

--- BankApi/Controllers/AccountQueryController.cs
+++ BankApi/Controllers/AccountQueryController.cs
@@ -12,11 +12,11 @@ namespace BankApi.Controllers
 
         public AccountQueryController(IAccountReader accountReader)
         {
-            AccountReader = accountReader ?? throw new System.ArgumentNullException(nameof(accountReader));
+            AccountReader = accountReader ?? throw new ArgumentNullException(nameof(accountReader));
         }
 
         [HttpGet]
-        [Route("/{accountId}")]
+        [Route("{accountId}")]
         public async Task<IActionResult> Get([FromRoute] Guid accountId)
         {
             var account = await AccountReader.GetAccountByIdAsync(accountId);
