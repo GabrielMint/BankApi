9f5163604698f3347acce1b0ba60de4e4a2db47b
Gabriel Augusto <augusto_costa96@hotmail.com>
2021-03-14 13:34:43 -0300

    Update Api to .Net5

--- BankApi/Startup.cs
+++ BankApi/Startup.cs
@@ -2,6 +2,8 @@ using BankApi.ServiceExtensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace BankApi
 {
@@ -10,12 +12,22 @@ namespace BankApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BankApi", Version = "v1" });
+            });
             services.ConfigureLog();
             services.ConfigureInfrastructure();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BankApi v1"));
+            }
+            
             app.UseRouting();
             app.UseEndpoints(e => e.MapControllers());
         }
